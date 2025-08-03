53e8fcf28357c94b7e70f972a672010f012584c4
Allen Farmer <james.farmer@dormakaba.com>
2025-08-03 12:10:04 -0500

    Update run.sh

--- wyzesense2mqtt/run.sh
+++ wyzesense2mqtt/run.sh
@@ -5,20 +5,20 @@ set -e
 # Write config.yaml for wyzesense2mqtt
 mkdir -p /app/config
 cat <<EOF > /app/config/config.yaml
-mqtt_host: "${MQTT_HOST}"
+mqtt_host: ${MQTT_HOST}
 mqtt_port: ${MQTT_PORT}
-mqtt_username: "${MQTT_USERNAME}"
-mqtt_password: "${MQTT_PASSWORD}"
-mqtt_client_id: "${MQTT_CLIENT_ID}"
+mqtt_username: ${MQTT_USERNAME}
+mqtt_password: ${MQTT_PASSWORD}
+mqtt_client_id: ${MQTT_CLIENT_ID}
 mqtt_clean_session: ${MQTT_CLEAN_SESSION}
 mqtt_keepalive: ${MQTT_KEEPALIVE}
 mqtt_qos: ${MQTT_QOS}
 mqtt_retain: ${MQTT_RETAIN}
-self_topic_root: "${SELF_TOPIC_ROOT}"
-hass_topic_root: "${HASS_TOPIC_ROOT}"
+self_topic_root: ${SELF_TOPIC_ROOT}
+hass_topic_root: ${HASS_TOPIC_ROOT}
 hass_discovery: ${HASS_DISCOVERY}
 publish_sensor_name: ${PUBLISH_SENSOR_NAME}
-usb_dongle: "${USB_DONGLE}"
+usb_dongle: ${USB_DONGLE}
 EOF
 
 # Write sensors_config to config/sensors.yaml if provided
